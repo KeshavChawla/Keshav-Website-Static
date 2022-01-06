4dd0320f5059778064d8e5d8c85e80a646f722a3
Keshav Chawla <keshavchawla2014@gmail.com>
2022-01-06 17:09:38 -0500

    update memoji and contentful

--- js/main.js
+++
@@ -1,4 +0,0 @@
-var client = contentful.createClient({
-  space: "<5k9v5bd8dfyz>",
-  accessToken: "<x-KhJxxCHZh-3Y8-sbprL_tHZIBsWYyVsQtisJPoa6g>",
-});
--- js/memoji.js
+++ js/memoji.js
@@ -1,14 +1,14 @@
 // Used to randomize emoji per page
 function getMemojiImage() {
   var memojiImage = "memoji-";
-  var memoji_integer = Math.floor(Math.random() * 34); // returns a random integer from 0 to 5
+  var memoji_integer = Math.floor(Math.random() * 33); // returns a random integer from 0 to 5
   memojiImage += memojiParseOut(memoji_integer);
   memojiImage += ".png";
   return memojiImage; //
 }
 // Check which specifc memojis to parse out
 function memojiParseOut(integer_passed) {
-  if (integer_passed == 10 || integer_passed == 30) {
+  if (integer_passed == 10 || integer_passed == 29) {
     return 3;
   } else if (integer_passed == 18 || integer_passed == 5) {
     return 26;
